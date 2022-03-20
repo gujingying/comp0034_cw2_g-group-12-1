a9f889a4ef6d129be691f85ba869b3ce0cecad86
gujingying <91874833+gujingying@users.noreply.github.com>
2022-03-20 14:37:27 +0000

    20/3

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 
 db = SQLAlchemy()
-login_manager = LoginManager()
+#login_manager = LoginManager()
 csrf = CSRFProtect()
 csrf._exempt_views.add('dash.dash.dispatch')
 #csrf_protect = CSRFProtect()
@@ -21,13 +21,21 @@ def create_app(config_class_name):
     app.config.from_object(config_class_name)
 
     db.init_app(app)
-    login_manager.init_app(app)
+    #login_manager.init_app(app)
     csrf.init_app(app)
 
 
     with app.app_context():
+        from my_app.models import User
+        db.create_all()
         # Import Dash application
         from dash_app.dash import init_dashboard
         app = init_dashboard(app)
+
+    from my_app.main.routes import main_bp
+    app.register_blueprint(main_bp)
+
+    from my_app.auth.routes import auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
